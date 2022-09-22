eeb28f30760b744d0c894437117e9521d8dbdea5
Calletje234 <calvinvanbeek123@outlook.com>
2022-09-22 15:21:57 +0200

    Created command for storing data filled in

--- UI/GetLocationImage/ScreenCompoments/bottom_buttons.py
+++ UI/GetLocationImage/ScreenCompoments/bottom_buttons.py
@@ -11,19 +11,34 @@ class BottomFrame:
     def create_frame(self):
         self.button_frame = QHBoxLayout()
 
-    def create_command_button(self, main):
+    def create_command_button(self, main, options_object):
         self.cancel_button.clicked.connect(lambda: self.close_app(main))
+        self.run_button.clicked.connect(lambda: self.set_options(options_object))
 
     def close_app(self, main):
         main.close()
+
+    def set_options(self, options_object):
+        self.options_list = []
+        latitude = options_object.get_lat_entrie()
+        longtitude = options_object.get_lon_entrie()
+        dimensions = options_object.get_dimensions_entrie()
+        date = options_object.get_date_entrie()
+        self.options_list.append(latitude)
+        self.options_list.append(longtitude)
+        self.options_list.append(dimensions)
+        self.options_list.append(date)
+
+    def get_options(self):
+        return self.options_list
     
     def add_buttons_to_frame(self):
         self.button_frame.addWidget(self.run_button)
         self.button_frame.addWidget(self.cancel_button)
 
-    def get_frame(self, main):
+    def get_frame(self, main, options_object):
         self.create_buttons()
         self.create_frame()
-        self.create_command_button(main)
+        self.create_command_button(main, options_object)
         self.add_buttons_to_frame()
         return self.button_frame
